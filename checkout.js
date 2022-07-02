1e990f20e81c7970a7f72d12d7e17324c1dfb528
Sophie Gilder <sophieachannon@hotmail.co.uk>
2022-07-02 17:06:07 +0100

    refactored checkout

--- checkout.js
+++ checkout.js
@@ -1,34 +1,32 @@
-class Shop {
+const items = {
+  'A' : 50,
+  'B' : 30,
+  'C' : 20,
+  'D' : 15
+}
 
+class Shop {
   checkout(string) {
     if (string.match(/[^A-D]/) != null) return -1;
-    string = string.split('').sort().join('');
-    let total = 0 + this.deals(string);
+    const itemArray = string.split('');
+    let total = 0 + this.deals(itemArray);
 
-    string.split('').forEach((item) => {
-      if (item === 'A') {
-        total += 50;
-      } else if (item === 'B') {
-        total += 30;
-      } else if (item === 'C') {
-        total += 20;
-      } else if (item === 'D') {
-        total += 15;
-      } else {
-        total =- 0
-      }
+    itemArray.forEach((item) => {
+      console.log(items[item])
+      total += parseInt(items[item])
     });
+    
     return total;
   };
 
-  deals(string) {
+  deals(items) {
     let reduction = 0;
-    let numberOfAs = string.split('').filter((item) => item === "A").length;
-    let numberOfBs = string.split('').filter((item) => item === "B").length;
-    if (string.includes("AAA")) {
+    let numberOfAs = items.filter((item) => item === "A").length;
+    let numberOfBs = items.filter((item) => item === "B").length;
+    if (numberOfAs >= 3) {
       reduction -= 20 * Math.floor(numberOfAs/3);
     }
-    if (string.includes("BB")) {
+    if (numberOfBs >= 2) {
       reduction -= 15 * Math.floor(numberOfBs/2);
     }
     return reduction;
